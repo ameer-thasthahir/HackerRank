d6df2f612dc2a3275bba7da1a9dcdd79b5b2d481
ameer.thasthahir <ameer.thasthahir@aspiresys.com>
2020-10-26 16:59:31 +0530

    Solved Few Problems

---
+++ MergeSort/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MergeSort
+{
+    class Program
+    {
+        public void MergeSort(List<int> arry, int leftStart, int rightEnd, int[] temp)
+        {
+            if (leftStart >= rightEnd)
+            {
+                return;
+            }
+
+            int middle = (leftStart + rightEnd) / 2;
+
+            MergeSort(arry, leftStart, middle, temp);
+
+            MergeSort(arry, middle + 1, rightEnd, temp);
+
+            MergeHalves(arry, leftStart, rightEnd, temp);
+
+        }
+
+        public void MergeHalves(List<int> arry, int leftStart, int rightEnd, int[] temp)
+        {
+            int leftEnd = (leftStart + rightEnd) / 2;
+            int rightStart = leftEnd + 1;
+            int left = leftStart;
+            int right = rightStart;
+            int index = leftStart;
+            int size = (rightEnd - leftStart) + 1;
+
+            while (left <= leftEnd && right <= rightEnd)
+            {
+                if (arry[left] < arry[right])
+                {
+                    temp[index] = arry[left];
+                    left++;
+                }
+                else
+                {
+                    temp[index] = arry[right];
+                    right++;
+
+                }
+
+                index++;
+            }
+
+            while (left <= leftEnd)
+            {
+                temp[index++] = arry[left++];
+            }
+
+            while (right <= rightEnd)
+            {
+                temp[index++] = arry[right++];
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                arry[rightEnd] = temp[rightEnd];
+                rightEnd--;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> arry = new List<int>() { 94,39,31,51,30,100,26,85,44,13,69,88,1,59,95,45,65,47,2,83,67,68,46,5,59,82,17,85,55,61,4,77,44,24,25,82,48,87,36,21,100,43,78,71,53,79,98,58,49,63,72,39,7,70,89,96,50,81,79,56,13,29,45,21,98,60,61,13,75,100,97,22,94,6,35,65,84,36,69,99,75,88,34,31,36,36,32,32,92,71,100,62,89,70,12,20,28,8,96,99};
+
+            int[] temp = new int[arry.Count];
+
+            new Program().MergeSort(arry, 0, arry.Count - 1, temp);
+
+            foreach (var item in temp)
+            {
+                Console.WriteLine(item);
+            }
+
+                                
+        }
+    }
+}
--- MultistageProblem/Program.cs
+++ MultistageProblem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MultistageProblem
 {
@@ -6,39 +7,71 @@ namespace MultistageProblem
     {
         public static void Main()
         {
-            int stages = 4;
-            int[] cost = new int[9];
-            int n = 8;
-            int[] d = new int[9];
-            int[] v = new int[9];
-            int min = 0;
-            int[,] c = new int[9, 9]
-            {
-            {0,0,0,0,0,0,0,0,0},
-            {0,0,2,1,3,0,0,0,0},
-            {0,0,0,0,0,2,3,0,0},
-            {0,0,0,0,0,6,7,0,0},
-            {0,0,0,0,0,6,8,9,0},
-            {0,0,0,0,0,0,0,0,6},
-            {0,0,0,0,0,0,0,0,4},
-            {0,0,0,0,0,0,0,0,5},
-            {0,0,0,0,0,0,0,0,0}
-            };
+            // int min = int.MaxValue;
+            // int[] cost = new int[9];
+            // int n = 8;
+            // int[] d = new int[9];
+            // int[] v = new int[9];
+            // int[,] c = new int[9, 9]
+            // {
+            // {0,0,0,0,0,0,0,0,0},
+            // {0,0,2,1,3,0,0,0,0},
+            // {0,0,0,0,0,2,3,0,0},
+            // {0,0,0,0,0,6,7,0,0},
+            // {0,0,0,0,0,6,8,9,0},
+            // {0,0,0,0,0,0,0,0,6},
+            // {0,0,0,0,0,0,0,0,4},
+            // {0,0,0,0,0,0,0,0,5},
+            // {0,0,0,0,0,0,0,0,0}
+            // };
 
-            cost[n] = 0;
+            // cost[n] = 0;
 
-            for (int i = n - 1; i <= n; i--)
-            {
-                for (int k = i + 1; k <= n; k++)
-                {
-                    if (c[i, k] != 0)
-                    {
-                        Console.WriteLine($"[{i}][{k}]");
-                    }
-                }
-            }
+            // for (int i = n - 1; i >= 1; i--)
+            // {
+            //     for (int k = i + 1; k <= n; k++)
+            //     {
+            //         if (c[i, k] != 0 && (c[i, k] + cost[k]) < min)
+            //         {
+            //             min = c[i, k] + cost[k];
+            //             d[i] = k;
+            //         }
+            //     }
+            //     cost[i] = min;
 
-            Console.Read();
+            // }
+
+            // Console.Read();
+            List<long> arr = new List<long>() { 41, 34, 46, 9, 37, 32, 42, 21, 7, 13, 1, 24, 3, 43, 2, 23, 8, 45, 19, 30, 29, 18, 35, 11 };            
+            int n = 250;
+            Console.Write(count(n,arr));
         }
+
+        static int count(int n, List<long> S)
+        {
+            // table[i] will be storing the  
+            // number of solutions for value i. 
+            // We need n+1 rows as the table  
+            // is constructed in bottom up manner 
+            // using the base case (n = 0) 
+            int[] table = new int[n + 1];
+            int m = S.Count;
+
+            // Base case (If given value is 0) 
+            table[0] = 1;
+
+            // Pick all coins one by one and  
+            // update the table[] values after  
+            // the index greater than or equal  
+            // to the value of the picked coin 
+            for (int i = 0; i < m; i++)
+                for (long j = S[i]; j <= n; j++)
+                    table[j] += table[System.Math.Abs(j - S[i])];
+
+            return table[n];
+        }
+      
+
+
     }
 }
